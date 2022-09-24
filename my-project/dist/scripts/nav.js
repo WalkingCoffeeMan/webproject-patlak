6cb59ba29e9388c8b74f418cbd9d395304c67105
onurpa <onur.patlak@stud.fh-campuswien.ac.at>
2022-09-25 01:09:36 +0200

    testing website

--- my-project/dist/scripts/nav.js
+++ my-project/dist/scripts/nav.js
@@ -5,7 +5,7 @@ document.write(
   '    <a class="navbar-brand" href="#">\n' +
   '      <img src="img\\mrobot.png" alt="MrRobot" width="300" height="240" class="pull-left">\n' +
   '    </a>\n' +
-    '      <script src="scripts\\floppydisks.js"></script>' +
+    '      <script src="/scripts/floppydisks.js"></script>' +
   '    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">\n' +
   '      <span class="navbar-toggler-icon"></span>\n' +
   '    </button>\n' +
@@ -19,16 +19,16 @@ document.write(
   '          <a class="nav-link" href="/pages/about.html"><h1 class="text-light">About</h1></a>\n' +
   '        </li>\n' +
   '        <li>\n' +
-  '          <a class="nav-link" href="../pages/services.html"><h1 class="text-light">Services</h1></a>\n' +
+  '          <a class="nav-link" href="/pages/services.html"><h1 class="text-light">Services</h1></a>\n' +
   '        </li>\n' +
     '        <li>\n' +
-    '          <a class="nav-link" href="../pages/favepisode.html"><h1 class="text-light">Stage2-Attack</h1></a>\n' +
+    '          <a class="nav-link" href="/pages/favepisode.html"><h1 class="text-light">Stage2-Attack</h1></a>\n' +
     '        </li>\n' +
     '        <li>\n' +
-    '          <a class="nav-link" href="../pages/buyfloppydisks.html"><h1 class="text-light">Floppy-Disks</h1></a>\n' +
+    '          <a class="nav-link" href="/pages/buyfloppydisks.html"><h1 class="text-light">Floppy-Disks</h1></a>\n' +
     '        </li>\n' +
     '        <li>\n' +
-    '          <a class="nav-link" href="../pages/contact.html"><h1 class="text-light">Contact</h1></a>\n' +
+    '          <a class="nav-link" href="/pages/contact.html"><h1 class="text-light">Contact</h1></a>\n' +
     '        </li>\n' +
   '      </ul>\n' +
   '\n' +
