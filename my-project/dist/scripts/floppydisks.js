080c42a4cb1e15066152abb0cf1ba10f0e4688bb
onurpa <onur.patlak@stud.fh-campuswien.ac.at>
2022-09-25 03:47:53 +0200

    testing website

--- my-project/dist/scripts/floppydisks.js
+++ my-project/dist/scripts/floppydisks.js
@@ -1,4 +1,8 @@
-
+var removeCartLink = "/scripts/";
+if(document.location.href.includes("pages"))
+{
+    removeCartLink= "../scripts/";
+}
 if(localStorage.getItem("cartDisabled") != 1)
 {
     if(localStorage.getItem("cart")!= null || localStorage.getItem("cart") != 0)
@@ -29,7 +33,7 @@ function init()
         '<p id="boughtFloppies" class="text-white"></p>\n' +
         '<p class="text-white"> Floppy-Disks in your cart !</p>\n' +
         '<button onclick="removeCart()" id="removeButton" >Remove</button>' +
-        '<script src="../scripts/removeCart.js"></script>' +
+        '<script src="'+removeCartLink+'../scripts/''removeCart.js"></script>' +
         '</container>')
     ;
 }
